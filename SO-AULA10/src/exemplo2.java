3311e923642881f5fe3c117d501ab1a3dc8ca4bf
panmarques <pamelasilva08@hotmail.com>
2017-04-24 11:28:03 -0300

    Add files via upload

---
+++ SO-AULA10/src/exemplo2.java
@@ -0,0 +1,39 @@
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class exemplo2 {
+	public static void main(String[] args )throws IOException{
+		Scanner entrada= new Scanner( new FileInputStream(FileDescriptor.in));
+		
+		PrintWriter saida= new PrintWriter (new FileOutputStream(FileDescriptor.out));
+		
+		PrintWriter arquivo = new PrintWriter("src\\filmes.txt");
+		
+		saida.println("Queremos saber seus 4 filmes favoritos!");
+		
+		saida.flush();
+		
+		for (int i = 0; i<4; i++){
+			saida.print("nome do filme: ");
+			saida.flush();
+			String nome = entrada.nextLine();
+			arquivo.println(nome);
+			
+		}
+		saida.println("Obirgado por participar da pesquisa!");
+		saida.println("Os dados foram gravados em src\\filmes.txt");
+		saida.flush();
+		
+		entrada.close();
+		saida.close();
+		arquivo.close();
+		
+	}
+	
+
+}
